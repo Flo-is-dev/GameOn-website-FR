8a96b34224f2f15352af744e2f5a0363727f86c9
Flo-le-dev <f.gironde.dev@gmail.com>
2023-08-20 21:21:08 +0200

    P4_V1.0.4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnSubmit = document.querySelector(".btn-submit");
-const firstName = document.getElementById("first");
-const lastName = document.getElementById("last");
-const email = document.getElementById("email");
-const quantity = document.getElementById("quantity");
-const radio1 = document.getElementById("location1");
-const checkbox1 = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -37,65 +31,32 @@ function launchModal() {
   });
 }
 
-// Effet de validation lorsqu'on valide le formulaire et vérification étape par étape
-
-btnSubmit.addEventListener("click", (e) => {
-  // Pour prevenir le comportement par défaut de l'input de type submit
-  e.preventDefault();
-
-  if (firstName.value.length < 3) {
-    firstName.parentElement.innerHTML += `<p  class="error"> Veuillez entrer 2 caractères ou plus.
-    </p>`;
-    console.log(firstName.value.length);
-  } else if (lastName.value.length < 3) {
-    lastError.innerText = "Veuillez entrer 2 caractères ou plus.";
-    console.log(lastName.value.length);
-  } else if (!email.value.match(/@/)) {
-    alert("Adresse mail non valide");
-  } else if (isNaN(birthdate.value) != true) {
-    birthdate.parentElement.innerHTML += `<p  class="error">Vous devez entrer votre date de naissance.
-    </p>`;
-  } else if (isNaN(quantity.value)) {
-    console.log("C'est PAS un Nombre");
-    console.log(quantity.value);
-  } else if (radio1.checked) {
-    console.log("radio1 coché");
-  } else if (!checkbox1.checked) {
-    checkbox1.parentElement.innerHTML += `<p class="error"> Vous devez vérifier que vous acceptez les termes et conditions.
-    </p>`;
-  } else {
-    document.querySelector(
-      ".btn-submit"
-    ).parentElement.innerHTML += `<p class="success"> Merci ! Votre réservation a été reçue.
-    </p>`;
-    console.log("TOUT EST BON");
-  }
-});
-
 // * ------------2nd CHANCE----------- **********************
 
 const inputs = document.querySelectorAll(
   "input[type=text],input[type=email],input[type=date],input[type=number],input[type=radio],input[type=checkbox]"
 );
 
-// console.log(inputs);
-
 const firstnameChecker = (value) => {
   const firstName = document.getElementById("first");
-  if (value.length <= 2) {
+  if (value.length <= 1) {
     firstName.parentElement.classList.add("error");
+    firstName.classList.add("error-input");
   } else {
     firstName.parentElement.classList.remove("error");
+    firstName.classList.remove("error-input");
   }
 };
 
 const lastnameChecker = (value) => {
   console.log(value);
   const lastName = document.getElementById("last");
-  if (value.length <= 2) {
+  if (value.length <= 1) {
     lastName.parentElement.classList.add("error");
+    lastName.classList.add("error-input");
   } else {
     lastName.parentElement.classList.remove("error");
+    lastName.classList.remove("error-input");
   }
 };
 
@@ -104,7 +65,9 @@ const emailChecker = (value) => {
   console.log(value);
   if (!value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
     email.parentElement.classList.add("error");
+    email.classList.add("error-input");
   } else {
+    email.classList.remove("error-input");
     email.parentElement.classList.remove("error");
   }
 };
@@ -113,28 +76,57 @@ const birthdateChecker = (value) => {
   console.log(value);
   const birthDate = document.getElementById("birthdate");
 
-  if (isNaN(value) != true) {
+  if (value === "") {
+    birthDate.classList.add("error-input");
     birthDate.parentElement.classList.add("error");
   } else {
+    birthDate.classList.remove("error-input");
     birthDate.parentElement.classList.remove("error");
   }
 };
 
 const playChecker = (value) => {
   console.log(value);
+  const quantity = document.getElementById("quantity");
+
+  if (isNaN(value) || value < 0) {
+    quantity.classList.add("error-input");
+    quantity.parentElement.classList.add("error");
+  } else {
+    quantity.classList.remove("error-input");
+    quantity.parentElement.classList.remove("error");
+  }
 };
 
 const cityChecker = (value) => {
   console.log(value);
+  const spanCity = document.querySelector(".span-city");
+
+  if (value !== true) {
+    spanCity.classList.add("error-input");
+    spanCity.classList.add("error");
+  } else {
+    spanCity.classList.remove("error-input");
+    spanCity.classList.remove("error");
+  }
 };
 
 const cgvChecker = (value) => {
+  const checkbox1 = document.getElementById("checkbox1");
   console.log(value);
+
+  if (value === false) {
+    checkbox1.classList.add("error-input");
+    checkbox1.parentElement.classList.add("error");
+  } else {
+    checkbox1.classList.remove("error-input");
+    checkbox1.parentElement.classList.remove("error");
+  }
 };
 
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
-    // console.log(e.target.id);
+    // console.log(e);
     // console.log(e.target.value);
     switch (e.target.id) {
       case "first":
@@ -158,14 +150,35 @@ inputs.forEach((input) => {
       case "location4":
       case "location5":
       case "location6":
-        cityChecker(e.target.value);
+        cityChecker(e.isTrusted);
         break;
       case "checkbox1":
-      case "checkbox2":
-        cgvChecker(e.target.value);
+        cgvChecker(e.target.checked);
         break;
       default:
-        nul;
+        null;
     }
   });
+});
+
+// Bouton qui permet de soumettre le formulaire. Affiche un message d'erreur si tous les champs ne sont pas valide
+btnSubmit.addEventListener("click", (e) => {
+  // Pour prevenir le comportement par défaut de l'input de type submit
+  e.preventDefault();
+
+  const modalBodySucced = document.querySelector(".modal-body-succed");
+
+  modalBodySucced.classList.remove("disable");
+});
+
+const btnEnd = document.getElementById("btn-end");
+
+// Boutton terminer qui permet de fermer la modal de confirmation d'inscription et de remttre le formulaire à zéro
+btnEnd.addEventListener("click", () => {
+  const modalBodySucced = document.querySelector(".modal-body-succed");
+  const myForm = document.getElementById("myForm");
+
+  modalbg.style.display = "none";
+  modalBodySucced.classList.add("disable");
+  myForm.reset();
 });
